5affcf44755a83a19ca3955c96ac02dc3ff04e8c
Wonsski <radek.rajda@hotmail.com>
2024-11-29 14:29:49 +0100

    Add sensor test for PM sensor

---
+++ sensor_tests/PM_sensor/PM_sensor.ino
@@ -0,0 +1,15 @@
+#include <PMserial.h>
+SerialPM pms(PMSx003, 16, 17);  // PMSx003, RX, TX
+
+void setup() {
+  Serial.begin(9600);
+  pms.init();                   // config serial port
+}
+
+void loop() {
+  pms.read();                   // read the PM sensor
+  Serial.print(F("PM1.0 "));Serial.print(pms.pm01);Serial.print(F(", "));
+  Serial.print(F("PM2.5 "));Serial.print(pms.pm25);Serial.print(F(", "));
+  Serial.print(F("PM10 ")) ;Serial.print(pms.pm10);Serial.println(F(" [ug/m3]"));
+  delay(10000);                 // wait for 10 seconds
+}
